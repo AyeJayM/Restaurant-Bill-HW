767cdf9c9e3bfaab279f7c0a83f47bd6b3393ddb
Austin Martinez <austinm@cpp.edu>
2021-09-29 19:23:40 -0700

    First Commit: Outline

---
+++ RestaurantBill.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+/* 
+1. Greet the user and briefly explain the program.
+
+2. Create random meal cost by assigning the four meals a value between 1 and 4. 
+        We will generate a random number between 1 and 4 and choose the 
+        corresponding meal.
+
+3. Prompt the user for tax and tip to be entered as a percent.
+        Multiply both by .01 to get the percent as a decimal.
+
+5. Multiply these two values by the total.
+
+6. Add these totals and print this sum to screen.
+        Also print the meal cost, tax, and tip individually.
+*/
+
