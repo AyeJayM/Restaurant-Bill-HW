eb141fe7271001a27623b1490351d8a665e99ea2
Austin Martinez <austinm@cpp.edu>
2021-09-29 20:39:51 -0700

    Bug Fixes, More Revised Version

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 /* 
@@ -17,6 +18,8 @@
         Also print the meal cost, tax, and tip individually.
 */
 
+int mealPicker();
+
 int mealPicker()
     {
         time_t t;
@@ -30,8 +33,14 @@ int mealPicker()
 
 int main()
 {
-    printf("Hello. This program will generate a restaurant bill with a little info provided by you.");
-    printf("\nFirst let's pick a random meal.");
+    printf("\nHello. This program will generate a restaurant bill with tax and tip provided by you.");
+
+    printf("\nFirst let's pick a random meal. The possible meals are...");
+
+    printf("\n\nSalad: $9.95");
+    printf("\nSoup: $4.55");
+    printf("\nSandwich: $13.25");
+    printf("\nPizza: $22.35\n");
 
     int meal;
     float mealCost;
@@ -41,22 +50,30 @@ int main()
     if (meal == 0) // Salad Option - $9.95
     {
         mealCost = 9.95;
+        printf("\nGenerated Meal: Salad\n");
     }
 
-    else if ( meal == 1) //Soup Option
+    else if ( meal == 1) //Soup Option - $4.55
     {
         mealCost = 4.55;
+        printf("\nGenerated Meal: Soup\n");
     }
 
-    else if ( meal == 2) //Sandwhich Option
+    else if ( meal == 2) //Sandwich Option - $13.25
     {
         mealCost = 13.25;
+        printf("\nGenerated Meal: Sandwich\n");
     }
 
-    else if (meal == 3) //Pizza
+    else if (meal == 3) //Pizza - $22.35
     {
         mealCost = 22.35;
+        printf("\nGenerated Meal: Pizza\n");
     }
+
+    printf("\nYour meal will cost $%.2f.\n", mealCost);
+    printf("\nPlease enter the amount you would like to tip as a percentage.");
+    printf("\nFor example, to enter a 20%% tip, simply enter \"20\"\n");
 
 
 
