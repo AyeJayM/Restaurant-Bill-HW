ad4b427dcd787b164870e7eba560345f94c39670
Austin Martinez <austinm@cpp.edu>
2021-09-29 19:58:50 -0700

    Added Meal Generator/Updated Comments

--- RestaurantBill.c
+++ RestaurantBill.c
@@ -4,8 +4,8 @@
 /* 
 1. Greet the user and briefly explain the program.
 
-2. Create random meal cost by assigning the four meals a value between 1 and 4. 
-        We will generate a random number between 1 and 4 and choose the 
+2. Create random meal cost by assigning the four meals a value between 0 and 3. 
+        We will generate a random number between 0 and 3 and choose the 
         corresponding meal.
 
 3. Prompt the user for tax and tip to be entered as a percent.
@@ -17,3 +17,51 @@
         Also print the meal cost, tax, and tip individually.
 */
 
+int mealPicker()
+    {
+        time_t t;
+
+        srand((unsigned)time(&t)); //Set our random generation based on time.
+
+        int entree = (rand() % 4); //This will generate a random number between 0 and 3.
+
+        return entree;
+    }
+
+int main()
+{
+    printf("Hello. This program will generate a restaurant bill with a little info provided by you.");
+    printf("\nFirst let's pick a random meal.");
+
+    int meal;
+    float mealCost;
+
+    meal = mealPicker();
+
+    if (meal == 0) // Salad Option - $9.95
+    {
+        mealCost = 9.95;
+    }
+
+    else if ( meal == 1) //Soup Option
+    {
+        mealCost = 4.55;
+    }
+
+    else if ( meal == 2) //Sandwhich Option
+    {
+        mealCost = 13.25;
+    }
+
+    else if (meal == 3) //Pizza
+    {
+        mealCost = 22.35;
+    }
+
+
+
+
+
+
+
+}
